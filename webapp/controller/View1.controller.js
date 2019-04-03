cc21a61cde032347bc8745feb9036ff544e5f555
Jayesh kulkarni <jayeshkulkarniofficial@gmail.com>
2019-04-03 08:08:31 +0000

    m

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("test.bindingtest.controller.View1", {
+		onInit: function () {
+
+		},
+		onRowClick:function(oEvent){
+			var oSelectedItem = oEvent.getSource();
+			var oContext = oSelectedItem.getBindingContext("Info");
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("SimpleFormChange354");
+			oProductDetailPanel.bindElement({ path: sPath, model: "Info" });
+		}
+	});
+});
