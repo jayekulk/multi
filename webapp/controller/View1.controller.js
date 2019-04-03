e1490e7c5aaca2ce289a25913f97f2965d467ec3
Jayesh Kulkarni <jayesh.kulkarni@capgemini.com>
2019-04-03 08:24:33 +0000

    c

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("test.bindingtest.controller.View1", {
 		onInit: function () {
-//no help fsad sada
+//no help 
 		},
 		onRowClick:function(oEvent){
 			var oSelectedItem = oEvent.getSource();
